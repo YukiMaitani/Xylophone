4f159603e06927b600998c4bbf3bfdb0d38a4fa5
YukiMaitani <fightzoi1997@gmail.com>
2022-06-14 14:06:51 +0900

    課題クリア

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,29 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
+    @IBAction func keyPressed(_ sender: UIButton) {
+        playSound(button: sender)
+    }
     
-
+    func playSound(button:UIButton) {
+        let url = Bundle.main.url(forResource: button.currentTitle!, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+        button.alpha /= 2
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            button.alpha *= 2
+        }
+    }
 }
+
 
